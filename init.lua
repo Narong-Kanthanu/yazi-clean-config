f0c886b7b225597a3273291ed5445fd64cc32a85
Narong <narong@flowaccount.com>
2025-04-15 11:25:08 +0700

    (fix): Update git plugin issue

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-require("git"):setup()
 require("full-border"):setup({ type = ui.Border.ROUNDED })
 require("no-status"):setup()
+require("git"):setup()
 
-THEME.git = THEME.git or {}
-THEME.git.added = ui.Style():fg("green")
-THEME.git.modified = ui.Style():fg("blue")
-THEME.git.deleted = ui.Style():fg("red"):bold()
-THEME.git.added_sign = "A"
-THEME.git.modified_sign = "M"
-THEME.git.deleted_sign = "D"
+th.git = th.git or {}
+th.git.added = ui.Style():fg("green")
+th.git.modified = ui.Style():fg("blue")
+th.git.deleted = ui.Style():fg("red"):bold()
+th.git.added_sign = "A"
+th.git.modified_sign = "M"
+th.git.deleted_sign = "D"
