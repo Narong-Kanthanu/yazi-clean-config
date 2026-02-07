6518ff3efe9ce2516d82e65e3e9201ddd7f8f8c8
Narong Kanthanu <narong.okan@gmail.com>
2026-02-07 10:58:05 +0700

    (fix): Update config to support config dir is exist

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 install_config() {
+
   local -r source_file=$(pwd)
   local -r destination_file=$HOME/.config/yazi
+
+  # Check if the directory already exists remove previous one
+  if [ -d "$destination_file" ]; then
+    echo "Directory $destination_file exists."
+    rm -r "$destination_file"
+  fi
+
   ln -s "$source_file" "$destination_file"
+  ya pkg install
   echo "Install success, please re-open Yazi."
 }
 
